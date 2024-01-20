2bc38fb69bc241c6aed4b16974b3bf986ff0cb62
Amaan Pathan <eulerbutcooler@gmail.com>
2024-01-20 13:02:19 +0530

    added the index.js path

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,16 @@ const {todo} = require('./db');
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path')
 
 app.use(express.json())
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+});
 
 app.post("/todo", async (req,res)=>{
     const createPayload = req.body;
